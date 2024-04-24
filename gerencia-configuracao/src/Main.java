099a13dc69258bdf83fa8389306d066b044ef417
Lucas Freitas <76873191+LucasFreitas00@users.noreply.github.com>
2024-04-24 14:32:37 -0300

    Utilizando classe 'Soma'

--- gerencia-configuracao/src/Main.java
+++ gerencia-configuracao/src/Main.java
@@ -1,5 +1,13 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Lucas da Silva Freitas");
+
+        Soma soma1 = new Soma(33, 12);
+        soma1.somar(soma1.getNum1(), soma1.getNum2());
+
+        Soma soma2 = new Soma();
+        soma2.somar(22, 14);
+
+        System.out.println("O valor da primeira soma é " + soma1.getResultadoSoma() + " e da segunda soma é " + soma2.getResultadoSoma());
     }
 }
